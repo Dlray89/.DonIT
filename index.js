b65964b691e902b93d3984f559e5eba4813f5e4e
DAP <dlrayjr89@gmail.com>
2020-07-03 15:39:32 +0000

    file clean up for deployment

--- index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-
-const server = require("./api/server")
-
-const PORT = process.env.PORT || 4000
-
-server.listen(PORT, () => {
-    console.log(`\n ======== Server is running on localhost:${PORT} ========== \n`)
-})
